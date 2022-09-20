df9b51fcd599f498c948d7f22b3e6b1c50cc2f3b
Fadlullah <fadlulanre@gmail.com>
2022-09-20 15:06:21 +0100

    added new fields to the task schema

--- models/task.js
+++ models/task.js
@@ -4,22 +4,33 @@ const TaskSchema = new mongoose.Schema({
     title: {
         type: String,
         required: [true, "must provide title"],
-        trim : true
+        trim: true
     },
     description: {
         type: String,
         required: [true, "must provide description"],
-        trim : true
+        trim: true
     },
-    timeStamp:{
-        type: Date,
-        default: Date.now()
-    },
-    createdBy:{
+    createdBy: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
         select: false
-    }
-})
+    },
+    deadlineDate : {
+        type: Date,
+        required : [true, "must provide deadline date"]
+    },
+    taskType : {
+        type : String,
+        enum : ["group", "personal"],
+        required : [true, "must provide task type"]
+    },
+    groupEmail : [{
+        type: String
+    }]
+},
+    { timestamps: true },
+
+)
 
 module.exports = mongoose.model("Task", TaskSchema)
