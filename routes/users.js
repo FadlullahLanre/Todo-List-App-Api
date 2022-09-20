17e8c9559cb9651dc7203366b9a96219c467a677
Ekene Anthony IkeOkoro <ekeneanthony85@gmail.com>
2022-09-20 14:33:09 +0100

    User features

--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,19 @@ const {
 	login,
 	forgotPassword,
 	resetPassword,
+	updatePassword,
 	confirmEmail,
 	protect,
-	logout
+	logout,
+	updateMe
 } = require('../controllers/users');
 
 
 router.route('/login').post(login);
 router.route('/signup').post(signup);
 router.route('/forgotPassword').post(forgotPassword);
+router.route('/updatePassword').post(updatePassword);
+router.route('/updateMe').post(updateMe);
 router.route('/resetPassword/:token').post(resetPassword);
 router.route('/confirmEmail/:token').get(confirmEmail);
 router.route('/logout').get(protect, logout);
